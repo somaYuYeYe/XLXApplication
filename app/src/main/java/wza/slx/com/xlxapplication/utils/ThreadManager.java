a6427f5617299c36b503837b1cadc7b71bc6b044
wangzc55 <wangzc55@ziroom.com>
2017-04-07 17:05:18 +0800

    thread manager not succ til now

---
+++ app/src/main/java/wza/slx/com/xlxapplication/utils/ThreadManager.java
@@ -0,0 +1,32 @@
+package wza.slx.com.xlxapplication.utils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by homelink on 2017/4/5.
+ */
+public class ThreadManager {
+    private static ThreadManager ourInstance = new ThreadManager();
+
+    public static ThreadManager getInstance() {
+        return ourInstance;
+    }
+
+    private ExecutorService cachedThreadPool;
+
+    private ThreadManager() {
+        cachedThreadPool = Executors.newCachedThreadPool();
+//        cachedThreadPool = Executors.newScheduledThreadPool()
+        cachedThreadPool = Executors.newFixedThreadPool(4);
+//        cachedThreadPool = new ThreadPoolExecutor(1, 4, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(4));
+        cachedThreadPool = new ThreadPoolExecutor(1, 4, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(4));
+    }
+
+    public void execute(Runnable task) {
+        cachedThreadPool.execute(task);
+    }
+}
